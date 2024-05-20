0c068169c562acd8fe507911e0caf1e541dd3feb
VishnuSusilkumar <vishnususilkumar10@gmail.com>
2024-05-20 09:38:44 +0530

    update header component, make profile private and update the useSelector

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,9 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import userReducer from "./user/userSlice";
-import { persistReducer } from "redux-persist";
+import userReducer from "./user/userSlice.js";
+import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import persistStore from "redux-persist/es/persistStore";
 
-const rootReducer = combineReducers({
-  user: userReducer,
-});
+const rootReducer = combineReducers({ user: userReducer });
 
 const persistConfig = {
   key: "root",
@@ -17,9 +14,7 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: persistedReducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
